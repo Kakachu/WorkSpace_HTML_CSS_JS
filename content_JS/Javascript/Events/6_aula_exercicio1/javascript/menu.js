d4ac0082f1afd0cf3d4ee1db7d3247ffdc6f971b
kakachu <kauajardim2004@hotmail.com>
2022-03-17 19:40:39 -0300

    menu update

--- content_JS/Javascript/Events/6_aula_exercicio1/javascript/menu.js
+++ content_JS/Javascript/Events/6_aula_exercicio1/javascript/menu.js
@@ -4,10 +4,17 @@
     var $html = document.querySelector('html');
     var $btn = document.querySelector('.header-nav__hamburgger');
 
+    var $ariaControl = $btn.getAttribute('aria-constrols');
+    var $menu = document.getElementById($ariaControl);
     $html.classList.remove('no-js');
     $html.classList.add('js');
 
-    $btn.addEventListener('click', function(){
+    $btn.addEventListener('click', function(){    
         $html.classList.toggle('menu-opened');
+        
+        var ariaExpanded = this.getAttribute('aria-expanded') == 'true';
+        $btn.setAttribute('aria-expanded', !ariaExpanded)
+        $menu.setAttribute('aria-expanded', !ariaExpanded)
+
     })
 })();
