f9f074d49a1d0b0d7b7fad70a3876afc31577a3c
kakachu <kauajardim2004@hotmail.com>
2022-03-16 23:13:38 -0300

    Events_Part_III_Final

---
+++ content_JS/Javascript/Events/6_aula_exercicio1/javascript/menu.js
@@ -0,0 +1,13 @@
+(function(){
+    'use strict'
+
+    var $html = document.querySelector('html');
+    var $btn = document.querySelector('.header-nav__hamburgger');
+
+    $html.classList.remove('no-js');
+    $html.classList.add('js');
+
+    $btn.addEventListener('click', function(){
+        $html.classList.toggle('menu-opened');
+    })
+})();
